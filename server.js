908cce83b40ecef1a2fb2b8b570f8b4204db3809
David Bresnick <david.bresnick@gmail.com>
2015-04-12 16:48:43 -0400

    Changed routing to Express routing

--- server.js
+++ server.js
@@ -7,28 +7,29 @@ var lastreq = '';
 // Set the app's port
 app.set('port', 80);
 
-app.use('/test', function(req,res) {
-	if(req.method == "POST") {
-		var requestBody = '';
-		req.on('data', function(data) {
-			requestBody += data;
-			if(requestBody.length > 1e7) {
-				res.writeHead(413, 'Request Entity Too Large', {'Content-Type': 'text/html'});
-          			res.end('<!doctype html><html><head><title>413</title></head><body>413: Request Entity Too Large</body></html>');
-			}
-		});
-		req.on('end', function() {
-			lastreq = JSON.parse(requestBody);
-			res.writeHead(200, { 'Content-type': 'text-plain'});
-			res.end('Accepted');
-		});
-	} else {
+app.route('/test')
+  .get(function(req, res) {
 	res.writeHead(200, { 'Content-type': 'text-plain'});
 	res.end('You made it to test!');
-	}
-});
+  })
+  .post(function(req, res) {
+	var requestBody = '';
+	req.on('data', function(data) {
+		requestBody += data;
+		if(requestBody.length > 1e7) {
+			res.writeHead(413, 'Request Entity Too Large', {'Content-Type': 'text/html'});
+			res.end('<!doctype html><html><head><title>413</title></head><body>413: Request Entity Too Large</body></html>');
+		}
+	});
+	req.on('end', function() {
+		lastreq = JSON.parse(requestBody);
+		res.writeHead(200, { 'Content-type': 'text-plain'});
+		res.end('Accepted');
+	});
+  });
 
-app.use(function(req,res) {
+app.route('/')
+  .get(function(req, res) {
 	res.writeHead(200, { 'Content-type': 'text-plain'});
 	if(lastreq == '') {
 		res.end('No request sent yet');
